adc1d706c666c8ca3d6bf0a04ae1a6ab0a42fcea
move37-000 <move37.000@gmail.com>
2026-05-21 10:19:50 +0900

    feat: errors

--- src/adapter/yfinance_market_news_fetcher.py
+++ src/adapter/yfinance_market_news_fetcher.py
@@ -26,6 +26,10 @@ class YFinanceMarketNewsFetcher(MarketNewsFetcher):
         self._news_limit = news_limit
 
     def fetch(self) -> list[NewsItem]:
+        # NetworkError/ParseError로 번역하지 않는 이유: 호출측이 분류할 가치가
+        # 없다(어차피 []로 끝남). 
+        # @retry를 달지 않는 이유: 예외가 밖으로 나가지 않으므로
+        # 데코레이터가 발동할 일이 없다.
         try:
             ticker = yf.Ticker(self._symbol)
             return parse_yfinance_news(ticker, self._news_limit)
